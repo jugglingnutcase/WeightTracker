792cd08bf4bd09a06deca665f227fc3bdd26047a
James R Sconfitto <jugglingnutcase@gmail.com>
2014-05-30 08:00:29 -0400

    :lipstick: Remove comment

--- app.js
+++ app.js
@@ -56,4 +56,3 @@ app.post('/weights/add', weights.addWeight);
 
 var port = process.env.PORT || 3000;
 app.listen(port);
-//console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
